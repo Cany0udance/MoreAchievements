9107ff0c4c7eaaf6f5ed1a9ef2600c89a49d6bab
Cany0udance <blakkrum@aol.com>
2024-06-19 00:05:26 -0400

    test

--- src/main/java/moreachievements/patches/statScreen/StatScreenScrollbarPatch.java
+++ src/main/java/moreachievements/patches/statScreen/StatScreenScrollbarPatch.java
@@ -1,6 +1,4 @@
 
-/*
-
 package moreachievements.patches.statScreen;
 
 import basemod.ReflectionHacks;
@@ -10,20 +8,14 @@ import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.screens.stats.StatsScreen;
 
 @SpirePatch2(
-        clz = StatsScreen.class,
-        method = "calculateScrollBounds"
+       clz = StatsScreen.class,
+       method = "calculateScrollBounds"
 )
 public class StatScreenScrollbarPatch {
-    public static Integer NEW_ACHIEVEMENTS = 23;
-
-    public StatScreenScrollbarPatch() {
-    }
-
-    @SpirePostfixPatch
-    public static void Postfix(StatsScreen __instance) {
-        float currentUpperBound = (Float) ReflectionHacks.getPrivate(__instance, StatsScreen.class, "scrollUpperBound");
-        ReflectionHacks.setPrivate(__instance, StatsScreen.class, "scrollUpperBound", currentUpperBound + (float)(36 * NEW_ACHIEVEMENTS) * Settings.scale);
-    }
+   @SpirePostfixPatch
+   public static void Postfix(StatsScreen __instance) {
+       float currentUpperBound = (Float) ReflectionHacks.getPrivate(__instance, StatsScreen.class, "scrollUpperBound");
+       float moreAchievementsHeight = StatsScreenPatch.getMoreAchievements().calculateHeight();
+       ReflectionHacks.setPrivate(__instance, StatsScreen.class, "scrollUpperBound", currentUpperBound + moreAchievementsHeight);
+   }
 }
-
- */
